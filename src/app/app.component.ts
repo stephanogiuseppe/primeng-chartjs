0bd181daaf860bca26ebccf7433f03e224a2e3ac
Stéphano Giuseppe <stephano.grp@gmail.com>
2018-05-01 20:03:10 -0300

    Angular with Chart.js - LineChart on app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {GrowlModule, Message} from 'primeng/primeng';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  data: any;
+
+  msgs: Message[];
+
+  constructor() {
+    this.data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40, 40, 60],
+          fill: false,
+          borderColor: '#4bc0c0'
+        },
+        {
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90, 88, 100],
+          fill: false,
+          borderColor: '#565656'
+        }
+      ]
+    };
+  }
+
+  selectData(event) {
+    this.msgs = [];
+    this.msgs.push({
+      severity: 'info',
+      summary: 'Data Selected',
+      detail: this.data.datasets[event.element._datasetIndex].data[
+        event.element._index
+      ]
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import {
+  MessagesModule,
+  GrowlModule
+} from 'primeng/primeng';
+import { ChartModule } from 'primeng/chart';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MessagesModule,
+    GrowlModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
